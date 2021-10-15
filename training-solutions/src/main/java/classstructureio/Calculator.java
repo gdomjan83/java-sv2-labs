cfa837a62a966934ef678f57815ac88a27e8d3fb
J_C <gomjan83@gmail.com>
2021-10-15 17:44:58 +0200

    Finished Calculator

--- training-solutions/src/main/java/classstructureio/Calculator.java
+++ training-solutions/src/main/java/classstructureio/Calculator.java
@@ -1,4 +1,17 @@
 package classstructureio;
 
+import java.util.Scanner;
+
 public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Kérlek adj meg egy számot!");
+        Scanner scanner = new Scanner(System.in);
+        int number1 = scanner.nextInt();
+        System.out.println("Adj meg még egy számot!");
+        int number2 = scanner.nextInt();
+        int total = number1 + number2;
+        System.out.println(number1 + " + " + number2);
+        System.out.println(total);
+
+    }
 }
