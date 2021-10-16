5565866409e4b4f25331e737abd791669c2ff9fd
J_C <gomjan83@gmail.com>
2021-10-16 15:04:11 +0200

    Created Book and BookMain

---
+++ training-solutions/src/main/java/classstructureconstructors/BookMain.java
@@ -0,0 +1,27 @@
+package classstructureconstructors;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class BookMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ki a könyv írója?");
+        String author = scanner.nextLine();
+        System.out.println("Mi a könyv címe?");
+        String title = scanner. nextLine();
+
+        Book book = new Book(author, title);
+
+        System.out.println("A kért könyv:");
+        System.out.println(book.getAuthor() + ": " + book.getTitle());
+
+        System.out.println("Add meg a könyv regisztrációs számát!");
+        String regNumber = scanner.nextLine();
+        book.register(regNumber);
+
+        System.out.println("A könyv adatai:");
+        System.out.println(book.getAuthor() + ": " + book.getTitle() + " (" + book.getRegNumber() + ")");
+    }
+}
