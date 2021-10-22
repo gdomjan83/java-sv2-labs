c7fd08011adb6aac05875dc6a34559fe0434a641
J_C <gomjan83@gmail.com>
2021-10-22 22:54:38 +0200

    Flowers excercise updated

--- training-solutions/src/main/java/cmdarguments/flowers/CmdMain.java
+++ training-solutions/src/main/java/cmdarguments/flowers/CmdMain.java
@@ -2,7 +2,10 @@ package cmdarguments.flowers;
 
 public class CmdMain {
     public static void main(String[] args) {
-        for (int i = 0; i < args.length; i++) {
+        int len = args.length;
+        System.out.println("Tömb hossza: " + len);
+
+        for (int i = 0; i < len; i++) {
             System.out.println((i + 1) + ". elem: " + args[i]);
         }
     }
