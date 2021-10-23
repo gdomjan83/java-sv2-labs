39d57c2b0281ad7cc36783ba2de761cb78b081aa
J_C <gomjan83@gmail.com>
2021-10-23 13:23:36 +0200

    Temperature comparison excercises finished

--- training-solutions/src/main/java/arrays/ArraysMain.java
+++ training-solutions/src/main/java/arrays/ArraysMain.java
@@ -6,9 +6,51 @@ import java.util.Arrays;
 public class ArraysMain {
     public String numberOfDaysAsString() {
         int[] numberOfDays = new int[12];
+
         for (int i = 0; i < numberOfDays.length; i++) {
             numberOfDays[i] = Month.of(i + 1).length(false);
         }
+
         return Arrays.toString(numberOfDays);
+    }
+
+    public String multiplicationTableAsString(int size) {
+        int[][] table = new int[size][];
+
+        for (int i = 0; i < table.length; i++) {
+            table[i] = new int[size];
+            for (int j = 0; j < size; j++) {
+                table[i][j] = (i + 1) * (j + 1);
+            }
+        }
+
+        return Arrays.deepToString(table);
+    }
+
+    public boolean sameTempValues(double[] day, double[] anotherDay) {
+        return Arrays.equals(day, anotherDay);
+    }
+
+    public int min(int value1, int value2) {
+        if (value1 <= value2) {
+            return value1;
+        } else {
+            return value2;
+        }
+    }
+
+    public boolean sameTempValuesDayLight(double[] day, double[] anotherDay) {
+        int dayLength = day.length;
+        int anotherDayLength = anotherDay.length;
+
+        int shorterLength = min(dayLength, anotherDayLength);
+
+        return Arrays.equals(Arrays.copyOfRange(day, 0, shorterLength), Arrays.copyOfRange(anotherDay, 0, shorterLength));
+    }
+
+    public boolean wonLottery(int[] playedNumbers, int[] winningNumbers) {
+
+
+        return true;
     }
 }
--- training-solutions/src/main/java/arrays/ArraysMainTest.java
+++ training-solutions/src/main/java/arrays/ArraysMainTest.java
@@ -4,6 +4,32 @@ public class ArraysMainTest {
     public static void main(String[] args) {
         ArraysMain arr = new ArraysMain();
 
-        System.out.println("Days of months:\n" + arr.numberOfDaysAsString());
+        System.out.println("Hónapok napjai:\n" + arr.numberOfDaysAsString());
+        System.out.println();
+
+        System.out.println("6-os szorzótábla:\n" + arr.multiplicationTableAsString(6));
+        System.out.println();
+
+        double[] day = {5.1, 5.5, 6.0, 7.3, 8.0, 10.2, 9.5,
+                10.6, 11.5, 15.6, 18.9, 20.0, 18.5, 16.3, 15.5,
+                15.0, 14.1, 13.0, 13.0, 12.0, 11.1, 11.0, 9.9, 9.9};
+        double[] anotherDay = {10.0, 5.5, 6.0, 7.3, 8.0, 10.2, 9.5,
+                10.6, 11.5, 15.6, 18.9, 20.0, 18.5, 16.3, 15.5,
+                15.0, 14.1, 13.0, 13.0, 12.0, 11.1, 11.0, 9.9, 9.9};
+
+        System.out.println("Napok összehasonlítása:\n" + arr.sameTempValues(day, anotherDay));
+        System.out.println();
+
+        double[] shortDay = {5.1, 5.5, 6.0, 7.3, 8.0, 10.2, 9.5,
+                10.6, 11.5, 15.6, 18.9, 20.0, 18.5, 16.3, 15.5,
+                15.0, 14.1, 13.0, 13.0, 12.0, 11.1, 11.0, 9.9, 9.9};
+        double[] longDay =  {5.1, 5.5, 6.0, 7.3, 8.0, 10.2, 9.5,
+                10.6, 11.5, 15.6, 18.9, 20.0, 18.5, 16.3, 15.5,
+                15.0, 14.1, 13.0, 13.0, 12.0, 11.1, 11.0, 9.9, 9.9, 8.0};
+
+        System.out.println("Eltérő hosszúságú napok összehasonlítása:\n" + arr.sameTempValuesDayLight(shortDay, longDay));
+        System.out.println();
+
+
     }
 }
