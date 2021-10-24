f834bdc948972a48546ce0ca7c768dfbef636ea3
J_C <gomjan83@gmail.com>
2021-10-24 14:19:09 +0200

    Debug excercises finished

---
+++ training-solutions/src/main/java/debug/StarMain.java
@@ -0,0 +1,11 @@
+package debug;
+
+public class StarMain {
+    public static void main(String[] args) {
+        Star star = new Star();
+
+        int[] numbers = {2, 5, 6, 8, 9, 13};
+        star.writeStars(numbers);
+    }
+
+}
