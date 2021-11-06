55520e7462467a5bc7e4ee4451fefc9edc0373fa
J_C <gomjan83@gmail.com>
2021-11-06 12:51:06 +0100

    Division excercise finished

---
+++ training-solutions/src/main/java/controliteration/division/Division.java
@@ -0,0 +1,36 @@
+package controliteration.division;
+
+
+public class Division {
+    private int number;
+    private int divider;
+
+    public Division(int number, int divider) {
+        this.number = number;
+        this.divider = divider;
+    }
+
+    public String generateResultWithFor() {
+        StringBuilder result = new StringBuilder();
+        for (int i = 1; i <= number; i++) {
+            if (i % divider == 0) {
+                result.append(i);
+                result.append(", ");
+            }
+        }
+        return result.toString();
+    }
+
+    public String generateResultWithWhile() {
+        StringBuilder result = new StringBuilder();
+        int i = 1;
+        while (i <= number) {
+            if (i % divider == 0) {
+                result.append(i);
+                result.append(", ");
+            }
+            i++;
+        }
+        return result.toString();
+    }
+}
