6b76a64997d92d6fc1924d6f0ef1f78d53d189de
J_C <gomjan83@gmail.com>
2021-11-13 17:11:49 +0100

    Adult excercise finished.

--- training-solutions/src/main/java/introexceptionthrow/Adult.java
+++ training-solutions/src/main/java/introexceptionthrow/Adult.java
@@ -1,5 +1,22 @@
 package introexceptionthrow;
 
 public class Adult {
+    private String name;
+    private int age;
 
+    public Adult(String name, int age) {
+        if (age < 18) {
+            throw new IllegalArgumentException(name + " nevű felhasználó 18 évesnél fiatalabb. Kilépés.");
+        }
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
---
+++ training-solutions/src/main/java/introexceptionthrow/AdultContent.java
@@ -0,0 +1,9 @@
+package introexceptionthrow;
+
+public class AdultContent {
+    public static void main(String[] args) {
+        Adult adult = new Adult("Zoli", 20);
+        Adult adult2 = new Adult("Gábor", 11);
+    }
+
+}
