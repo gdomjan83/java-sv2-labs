2d9191f9373ef56f880fd72ef6baf0477eec61a7
J_C <gomjan83@gmail.com>
2021-11-23 17:45:07 +0100

    Algorithm sum excercise1 finished.

---
+++ training-solutions/src/main/java/algorithmssum/school/School.java
@@ -0,0 +1,14 @@
+package algorithmssum.school;
+
+import java.util.List;
+
+public class School {
+    public int getNumberOfStudents(List<Integer> headcounts) {
+        int sum = 0;
+        for (Integer actual : headcounts) {
+            sum += actual;
+        }
+        return sum;
+    }
+
+}
---
+++ training-solutions/src/test/java/algorithmssum/school/SchoolTest.java
@@ -0,0 +1,22 @@
+package algorithmssum.school;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SchoolTest {
+
+    @Test
+    void testGetNumberOfStudents() {
+        School school = new School();
+
+        List<Integer> headcounts = Arrays.asList(25, 32, 18, 26);
+        int total = school.getNumberOfStudents(headcounts);
+
+        assertEquals(101, total);
+    }
+
+}
