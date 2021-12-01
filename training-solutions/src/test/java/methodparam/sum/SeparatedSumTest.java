84b8ef8b79ee04260b6277bac792ac07026727d2
J_C <gomjan83@gmail.com>
2021-12-01 20:14:26 +0100

    Methodparam excercise2 updated.

--- training-solutions/src/test/java/methodparam/sum/SeparatedSumTest.java
+++ training-solutions/src/test/java/methodparam/sum/SeparatedSumTest.java
@@ -17,4 +17,10 @@ class SeparatedSumTest {
         assertEquals(110.6, summedNumbers.getPositiveNumberSum(), 0.0001);
         assertEquals(-153.3, summedNumbers.getNegativeNumbersSum(), 0.0001);
     }
+
+    @Test
+    void testWrongPath() {
+        IllegalArgumentException iea = assertThrows(IllegalArgumentException.class,
+                () -> separatedSum.sum(separatedSum.readFile(Paths.get("src/test/resources/floatingnu.txt"))));
+    }
 }
