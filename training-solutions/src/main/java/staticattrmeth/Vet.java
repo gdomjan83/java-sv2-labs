30d3717f6889d33ef501beb837a4c3d180396c98
J_C <gomjan83@gmail.com>
2021-12-02 21:54:05 +0100

    Static attributes excercise 1 finished.

---
+++ training-solutions/src/main/java/staticattrmeth/Vet.java
@@ -0,0 +1,23 @@
+package staticattrmeth;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Vet {
+    private List<Dog> dogs = new ArrayList<>();
+    private static long code = 1;
+
+    public List<Dog> getDogs() {
+        return dogs;
+    }
+
+    public static long getCode() {
+        return code;
+    }
+
+    public void addDog(Dog dog) {
+        dog.setCodeNumber(code);
+        code++;
+        dogs.add(dog);
+    }
+}
