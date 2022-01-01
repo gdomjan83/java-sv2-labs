abe59fdfb77faaadbbeb59dc1fc738eaf9f0a4d7
J_C <gomjan83@gmail.com>
2022-01-01 22:47:31 +0100

    Schoolrecords console implemented.

--- training-solutions/src/main/java/schoolrecords/SchoolRecordsController.java
+++ training-solutions/src/main/java/schoolrecords/SchoolRecordsController.java
@@ -18,7 +18,7 @@ public class SchoolRecordsController {
         Subject subject1 = new Subject("Analízis");
         Subject subject2 = new Subject("Matematika");
         Subject subject3 = new Subject("Üzleti tervezés");
-        Subject subject4 = new Subject("Számvitel tervezés");
+        Subject subject4 = new Subject("Számvitel");
         subjectList = List.of(subject1, subject2, subject3, subject4);
         tutorList = List.of(
                 new Tutor("Vadvári Tibor", List.of(subject1)),
@@ -28,7 +28,7 @@ public class SchoolRecordsController {
 
     public void showMenu() {
         System.out.println(
-            "1. Diákok nevének listázása\n" +
+            "\n1. Diákok nevének listázása\n" +
             "2. Diák név alapján keresése\n" +
             "3. Diák létrehozása\n" +
             "4. Diák név alapján törlése\n" +
@@ -50,11 +50,11 @@ public class SchoolRecordsController {
         switch(option) {
             case "1":
                 System.out.println("Diákok:");
-                listNames();
+                System.out.println(listNames());
                 return true;
             case "2":
                 System.out.println("Add meg a keresett diák nevét:");
-                searchByName(getInput());
+                System.out.println(searchByName(getInput()));
                 return true;
             case "3":
                 System.out.println("Add meg az új diák nevét:");
@@ -65,17 +65,27 @@ public class SchoolRecordsController {
                 deleteStudent(getInput());
                 return true;
             case "5":
-
+                doRepetition();
                 return true;
             case "6":
+                System.out.print("Osztályátlag: ");
+                System.out.print(calculateClassAverage());
                 return true;
             case "7":
+                System.out.println("Melyik tantárgy átlagát szeretnéd látni?");
+                System.out.println(calculateSubjectAverage(getInput()));
+                System.out.println("Tantárgy átlag:");
                 return true;
             case "8":
+                studyResults();
                 return true;
             case "9":
+                System.out.println("Melyik diák átlagát szeretnéd látni?");
+                System.out.println(studentAverage(getInput()));
                 return true;
             case "10":
+                System.out.println("Melyik diák átlagát szeretnéd látni?");
+                System.out.println(studentSubjectAverage(getInput()));
                 return true;
             case "11":
                 return false;
@@ -84,12 +94,12 @@ public class SchoolRecordsController {
         }
     }
 
-    private void listNames() {
-        classRecord.listStudentNames();
+    private String listNames() {
+        return classRecord.listStudentNames();
     }
 
-    private void searchByName(String name) {
-        System.out.println(classRecord.findStudentByName(name).toString());
+    private String searchByName(String name) {
+        return classRecord.findStudentByName(name).toString();
     }
 
     private void createStudent(String name) {
@@ -100,6 +110,63 @@ public class SchoolRecordsController {
     private void deleteStudent(String name) {
         Student student = classRecord.findStudentByName(name);
         classRecord.removeStudent(student);
+    }
+
+    private void doRepetition() {
+        Student student = classRecord.repetition();
+        System.out.println("Felelő diák: " + student.getName());
+        System.out.println("Felelés eredménye (A-B-C-D-F):");
+        String markValue = getInput();
+        System.out.println("Oktató neve:");
+        String tutor = getInput();
+        System.out.println("Tantárgy neve:");
+        String subject = getInput();
+        Mark mark = new Mark(MarkType.valueOf(markValue), findSubjectByName(subject), findTutorByName(tutor));
+        student.grading(mark);
+    }
+
+    private double calculateClassAverage() {
+        return classRecord.calculateClassAverage();
+    }
+
+    private double calculateSubjectAverage(String subject) {
+        return classRecord.calculateClassAverageBySubject(findSubjectByName(subject));
+    }
+
+    private void studyResults() {
+        for (StudyResultByName s : classRecord.listStudyResults()) {
+            System.out.println(s.toString());
+        }
+    }
+
+    private double studentAverage(String name) {
+        Student student = classRecord.findStudentByName(name);
+        return student.calculateAverage();
+    }
+
+    private double studentSubjectAverage(String name) {
+        Student student = classRecord.findStudentByName(name);
+        System.out.println("Melyik tantárgy átlagát szeretnéd látni?");
+        String subject = getInput();
+        return student.calculateSubjectAverage(findSubjectByName(subject));
+    }
+
+    private Subject findSubjectByName(String name) {
+        for (Subject s : subjectList) {
+            if (name.equals(s.getSubjectName())) {
+                return s;
+            }
+        }
+        throw new IllegalArgumentException("This subject is not taught.");
+    }
+
+    private Tutor findTutorByName(String name) {
+        for (Tutor t: tutorList) {
+            if (name.equals(t.getName())) {
+                return t;
+            }
+        }
+        throw new IllegalArgumentException("Tutor by this name does not exist.");
     }
 
     public static void main(String[] args) {
