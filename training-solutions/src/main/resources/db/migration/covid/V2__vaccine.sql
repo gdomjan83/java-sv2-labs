c0a1302b766cb53c7127f631cdaacc9841613665
J_C <gomjan83@gmail.com>
2022-04-05 20:45:52 +0200

    Covid project sql migration files

--- training-solutions/src/main/resources/db/migration/covid/V2__vaccine.sql
+++ training-solutions/src/main/resources/db/migration/covid/V2__vaccine.sql
@@ -1,2 +1,3 @@
-create table vaccinations(vaccination_id bigint not null, citizen_id int not null, vaccination_date datetime not null,
-status varchar(10), note varchar(200), vaccination_type varchar(20), constraint pk_vaccine primary key(vaccination_id));
+create table vaccinations(vaccination_id bigint not null, citizen_id bigint not null, vaccination_date datetime not null,
+status varchar(10), note varchar(200), vaccination_type varchar(20), primary key(vaccination_id),
+foreign key (citizen_id) references citizens (citizen_id));
